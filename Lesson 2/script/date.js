4650f0c474059926882d94d835e9db7876e582d8
sheddycole <Enter your email address>
2022-04-30 19:25:33 -0400

    Assignment

---
+++ Lesson 2/script/date.js
@@ -0,0 +1,10 @@
+/*Last date modified code*/
+const lastmodified = document.querySelector("#lastmodified");
+lastmodified.innerHTML = `Last Updated: ${document.lastModified}
+//Get year code
+const p_year = document.querySelector("#elerunndu");
+const today = new Date();
+const year = today.getFullYear();
+p_year.innerHTML = ` & copy;
+`${year}`
+`Godwin Iyip`;
