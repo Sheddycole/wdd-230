919dd28bd2a6b89405ae54d4425987bbca9f689c
sheddycole <Enter your email address>
2022-05-01 09:20:06 -0400

    i made some changes to my assignment

--- Lesson 2/script/date.js
+++ Lesson 2/script/date.js
@@ -2,9 +2,9 @@
 const lastmodified = document.querySelector("#lastmodified");
 lastmodified.innerHTML = `Last Updated: ${document.lastModified}
 //Get year code
-const p_year = document.querySelector("#elerunndu");
+const p_year = document.querySelector("#Chinemerem");
 const today = new Date();
 const year = today.getFullYear();
 p_year.innerHTML = ` & copy;
 `${year}`
-`Godwin Iyip`;
+`Ukeje Chinemerem`;
