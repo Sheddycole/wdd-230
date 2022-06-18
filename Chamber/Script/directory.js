44aeaf43d7ceae24422a896645505197cd39a815
Sheddycole <90576786+Sheddycole@users.noreply.github.com>
2022-06-18 12:00:37 -0400

    development

--- Chamber/Script/directory.js
+++ Chamber/Script/directory.js
@@ -1,112 +1,29 @@
-const cardList = document.querySelector(".card-list");
-const directoryURL = "./data/data.json";
-const medium = window.matchMedia("(min-width:545px) and (max-width: 1079px)");
+const directoryUrl = "https://sheddycole.github.io/wdd230/Chamber/data/data.json";
+const directoryCont = document.querySelector(".directory-container");
 
-let width = window.innerWidth;
+async function getandshowDirectory() {
+    const response = await fetch(directoryUrl);
+    const jsonObject = await response.json();
+    const directory = jsonObject.directory;
+    console.log(directory);
 
-function make_cards(business) {
-  //Create cards from each item of the fetched list
-  return `<div class="card">
-            <img src = ${business.imageurl} alt = ${business.name} loading="lazy">
-            <address>${business.street}, ${business.city}</address>
-            <p>${business.number}</p>
-            <a href="${business.website}" target="_blank">${business.website}</a>
-        </div>`;
-}
-
-function make_list(business) {
-  //Create list items from each item of the fetched list
-
-  return `
-    <tr>
-    <td class="name">${business.name}</td>
-    <td class="list-address">${business.street},<br>${business.city}</td>
-    <td class="list-number">${business.number}</td>
-    <td><a href="${business.website}" target="_blank">${business.website}</a></td>
-    </tr>`;
-}
-
-class setDefView {
-  // initialzes with a def view then the method setView checks to set a default view on resize but prevent the chrome mobile
-  // scroll resize from triggering the resize event
-
-  constructor(medium, list) {
-    this.list = list;
-
-    if (medium.matches) {
-      // If media query matches
-      listView(this.list);
-    } else {
-      gridView(this.list);
-    }
-  }
-
-  setView(medium) {
-    //checks the viewport and set the defailt view either grid or list
-    if (window.innerWidth != width) {
-      if (medium.matches) {
-        // If media query matches
-        listView(this.list);
-      } else {
-        gridView(this.list);
-      }
-    }
-  }
-}
-
-// Fetch the JSON data for the directory
-fetch(directoryURL)
-  .then((response) => {
-    return response.ok ? response.json() : console.log("error");
-  })
-  .then((data) => {
-    let businesses = data["businesses"];
-
-    //A calls the funtion that sets the default view for medium screen as list ans leaves the rest as cards
-    const defView = new setDefView(medium, businesses);
-    defView.setView(medium);
-    window.addEventListener("resize", () => defView.setView(medium));
-
-    //event listener for button click
-    document
-      .querySelector("#grid-view")
-      .addEventListener("click", () => gridView(businesses));
-    document
-      .querySelector("#list-view")
-      .addEventListener("click", () => listView(businesses));
-  });
-
-function gridView(business) {
-  // change button color to currently active
-  let gridBtn = document.querySelector("#grid-view");
-  let listBtn = document.querySelector("#list-view");
-
-  gridBtn.style.backgroundColor = "white";
-  listBtn.style.backgroundColor = "#A9D6E5";
-
-  //sends the grid view to the html page
-  cardList.innerHTML = "";
-  const card = business.map(make_cards);
-  const cards = document.createElement("section");
-  cards.innerHTML = card.join(" ");
-  cards.className = "cards";
-  cardList.appendChild(cards);
-}
-
-function listView(business) {
-  // change button color to currently active
-  let gridBtn = document.querySelector("#grid-view");
-  let listBtn = document.querySelector("#list-view");
-
-  gridBtn.style.backgroundColor = "#A9D6E5";
-  listBtn.style.backgroundColor = "white";
-  //sends the list view to the html page
-
-  cardList.innerHTML = "";
-
-  const list = business.map(make_list);
-  const items = document.createElement("tbody");
-  items.innerHTML = list.join(" ");
-  items.className = "list";
-  cardList.appendChild(items);
-}
+    directoryCont.innerHTML = "";
+    directory.forEach((company) => {
+        directoryCont.insertAdjacentHTML("beforeend", 
+        `
+                    <div class="company abel-text">
+                        <img class="company-img" src="${company.logoURL}" loading="lazy" alt="logo of ${company.name}">
+                        <div class="company-info">
+                            <h2 class="mono-text">${company.name} <img src="images/star_FILL0_wght400_GRAD0_opsz24.svg" alt="${company.membership} tag" class="${company.membership}"></h2>
+                            <ul>
+                            <li>${company.email}</li>
+                            <li>${company.websiteURL}</li>
+                            <li>${company.address}</li>
+                            <li>${company.phoneNumber}</li>
+                            </ul>
+                            <p class="transparent">${company.description}</p>
+                        </div>
+                    </div>`)
+    });
+};
+getandshowDirectory();
