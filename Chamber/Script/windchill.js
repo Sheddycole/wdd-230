45266151fb4858a6a264383544cf83f3d1bbbbd0
Sheddycole <90576786+Sheddycole@users.noreply.github.com>
2022-06-24 08:00:19 -0400

    weather

--- Chamber/Script/windchill.js
+++ Chamber/Script/windchill.js
@@ -1,39 +1,39 @@
-function chills(cTemp, kmSpeed) {
-    let fTemp = cTemp * (9 / 5) + 32;
-  
-    let mSpeed = kmSpeed * 0.621371;
-  
-    if (fTemp <= 50 && mSpeed > 3) {
-      let fChill =
-        35.74 +
-        0.6215 * fTemp -
-        35.75 * Math.pow(mSpeed, 0.16) +
-        0.4275 * fTemp * Math.pow(mSpeed, 0.16);
-      let chill = (fChill - 32) * (5 / 9);
-  
-      document.querySelector(
-        "#chill-output"
-      ).textContent = `Wind Chill: ${chill.toFixed(1)}°C`;
-    } else {
-      document.querySelector("#chill-output").textContent = `Wind Chill: N/A`;
+const windSpeed = document.querySelector("#wind-speed");
+const windchill = document.querySelector("#wind-chill");
+const temperature = document.querySelector("#temp");
+const weatherIcon = document.querySelector("#weather-icon")
+const locationWeather = document.querySelector("#location-weather")
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Lagos&units=metric&appid=9759bf8ad638b8f6a851effbef8116f6"
+
+
+
+async function getWeather(){
+    const weatherFetch = await fetch(weatherURL);
+    const weatherJson = await weatherFetch.json()
+    console.log(weatherJson)
+    const windSpeedjson = weatherJson.wind.speed;
+    const temperatureC = weatherJson.main.temp
+    
+    const weatherLocation = weatherJson.name
+    const weatherType = weatherJson.weather[0].main
+
+    temperature.textContent = Math.round(temperatureC)
+    windSpeed.textContent = windSpeedjson
+    locationWeather.textContent = `${weatherLocation} - ${weatherType}`
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/w/${weatherJson.weather[0].icon}.png`)
+}
+
+function calcChill(){
+    const mph = Number(windSpeed.textContent) / 1.609;
+    const fahrenheit = (Number(temperature.textContent) * (9/5)) + 32
+    // (<=50°F and >3.0mph)
+    if((mph > 3) && (fahrenheit <= 50)){
+        const value = 35.74 + (0.6215 * fahrenheit) - (35.75 * (mph ** 0.16)) + (0.4275 * fahrenheit * (mph ** 0.16));
+        windchill.textContent = `${Math.round(value)}°F`
+    }else{
+        windchill.textContent = "N/A"
     }
-  }
-  
-  const apiURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=Uyo&units=metric&appid=1da99388e86d2e97825559d74b18e860";
-  
-  fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-      document.querySelector("#temp").textContent = jsObject.main.temp;
-      document.querySelector("#speed").textContent = jsObject.wind.speed;
-  
-      const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-      const desc = jsObject.weather[0].description;
-  
-      document.querySelector("#icon").setAttribute("src", iconsrc);
-      document.querySelector("#icon").setAttribute("alt", desc);
-      document.querySelector("#icon-desc").textContent = desc;
-  
-      chills(jsObject.main.temp, jsObject.wind.speed);
-    });
+}
+
+getWeather()
+calcChill()
